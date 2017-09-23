084511841914e0ac7bfe890ba1862568ae79b7a8
Brett Tucker <bretttucker@gmail.com>
2017-09-23 15:11:09 -0600

    Add dependencies

--- app/src/main/java/com/abnormallydriven/paginglibrarytestdrive/teamlist/TeamListActivity.kt
+++ app/src/main/java/com/abnormallydriven/paginglibrarytestdrive/teamlist/TeamListActivity.kt
@@ -2,11 +2,14 @@ package com.abnormallydriven.paginglibrarytestdrive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
