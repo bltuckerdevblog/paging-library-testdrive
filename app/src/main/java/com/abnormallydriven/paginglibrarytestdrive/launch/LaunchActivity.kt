9f210bd866cbefe8c33e1de1c9684d695f39faee
Brett <bretttucker@gmail.com>
2018-05-26 07:46:29 -0700

    Add new LaunchActivity

---
+++ app/src/main/java/com/abnormallydriven/paginglibrarytestdrive/launch/LaunchActivity.kt
@@ -0,0 +1,33 @@
+package com.abnormallydriven.paginglibrarytestdrive.launch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.abnormallydriven.paginglibrarytestdrive.R
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+
+        findViewById<Button>(R.id.positional_data_source)
+                .setOnClickListener { v ->
+                    //launch activity for positional data source
+                }
+
+
+        findViewById<Button>(R.id.paged_key_data_source)
+                .setOnClickListener { v ->
+                    //launch activity for paged key data source
+                }
+
+        findViewById<Button>(R.id.item_keyed_data_source)
+                .setOnClickListener { v ->
+                    //launch activity for item keyed data source
+                }
+
+
+    }
+}
